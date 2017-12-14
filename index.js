df360bb4bddd45b349f06aafb7c98a20b75f36bb
Kalin Bowden <kalinbowden@gmail.com>
2017-12-13 19:47:18 -0600

    Remade repo to fix errror with original git ignore

---
+++ index.js
@@ -0,0 +1,156 @@
+/*
+    Desc:   The logic js for the Electron app.
+    Dev:    Kalin Bowden.
+    Date: 11.29.2017.
+*/
+
+//
+const electron = require('electron');
+var app = require('electron').remote;
+var dialog = app.dialog;
+var fs =  require('fs');
+const ipc = electron.ipcRenderer;
+console.log('index.js has loaded succsefuly');
+
+
+
+/*document.getElementById('start').addEventListener('click', _=> {
+    ipc.send('countdown-start');
+});*/
+
+ipc.on('countdown', (evt, count) =>
+{
+    document.getElementById('count').innerHTML = count;
+})
+
+/*document.getElementById('btn1').addEventListener('click', saveFile);*/
+
+function saveFile()
+{
+    dialog.showSaveDialog((filename) => {
+        if (filename === undefined)
+        {
+            alert('You must give a file a name.');
+            return;
+        }
+
+        //
+        fs.writeFile(filename, createJSONTask(), (err)=>{
+            if(err)
+            {
+                console.log(err)
+            }
+            alert("The file has been saved!");
+        })
+    });
+}
+
+// Create JSON string
+function createJSONTask()
+{
+    //
+    var data = {}
+    data.table = [];
+    var JsonData;
+    var tasks = document.getElementById('task');
+
+    //
+    for (var i = 0; i < task.childElementCount; i++)
+    {
+        console.log(i);
+        console.log(task.children[i].children[0].children[0].innerHTML);
+        
+        data.table.push({'task': task.children[i].children[0].children[0].innerHTML});
+    }
+
+    //
+    JsonData = JSON.stringify(data);
+    console.log(JsonData.toString());
+
+    return JsonData;
+}
+
+//
+function openFile()
+{
+    dialog.showOpenDialog((filenames) => {
+        if(filenames === undefined)
+        {
+            alert("No files were selected");
+            return;
+        }
+
+        readFile(filenames[0]);
+    })
+}
+
+// JSOn dash file
+function readFile(filepath)
+{
+    fs.readFile(filepath, 'utf-8', (err,data) =>{
+        if(err){
+            alert('Oops... Looks like something went wrong');
+            return;
+        }
+
+        //
+        console.log('Results: ' + data);
+        temp = JSON.parse(data);
+        console.log(temp);    
+        
+        for (var i = 0; i < temp.table.length; i++)
+        {
+            console.log('try again: ' + temp.table[i].task);
+
+            var li = document.createElement('li');
+            li.classList.add('collection-item');
+            li.classList.add('z-depth-5');
+            li.classList.add('task');
+            li.innerHTML = 
+
+            '<div class="row">' +
+                        '<div class="col s6">'+temp.table[i].task+'</div>' +
+                        '<div class="col s1"></div>' +
+                        '<div class="col s1"></div>' +
+                        '<div class="col s4"></div>' +
+                    '</div>'
+                    
+            document.getElementById('task').appendChild(li);
+        }
+    })
+}
+
+//----------------------------------------------------------------------
+document.getElementById('add').addEventListener('click', _=>{
+    ipc.send('addTask');
+});
+
+document.getElementById('clear').addEventListener('click', _=>{
+    ipc.send('item:clear');
+});
+
+document.getElementById('close').addEventListener('click', _=>{
+    ipc.send('app:close');
+});
+
+document.getElementById('account').addEventListener('click', _=>{
+    ipc.send('app:account');
+});
+
+document.getElementById('download').addEventListener('click', _=>{
+    ipc.send('task:download');
+    openFile();
+});
+
+document.getElementById('upload').addEventListener('click', _=>{
+    ipc.send('task:upload');
+    saveFile();
+});
+
+document.getElementById('refresh').addEventListener('click', _=>{
+    ipc.send('app:refresh');
+});
+
+document.getElementById('voice').addEventListener('click', _=>{
+    ipc.send('app:voice');
+});
---
+++ main.js
@@ -0,0 +1,148 @@
+/*
+    Desc:   The main js for the Electron app.
+    Dev:    Kalin Bowden.
+    Date: 11.29.2017.
+*/
+
+// Constants for the instance of the Electron window
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+const ipc = electron.ipcMain;
+
+//
+let mainMenu;
+let addWindow;
+let mainWindow;
+
+//
+
+
+//  Create the main GUI window for the app
+app.on('ready', _=>{
+    // Starting-up notification
+    console.log('TaskList has started');
+
+    // Create the window
+    mainWindow = new BrowserWindow({ width:775, height:475, transparent: true, frame: true, toolbar: true});
+
+    //
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    //
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    // Destroy window
+    mainWindow.on('closed', _=>{mainWindow = null; console.log('TaskList has closed');})
+
+
+})
+
+
+//
+
+
+
+//
+//mainWindow.on('closed', _=>{app.quit();});
+
+// Garbage Collection
+//addWindow.on('closed', _=>{addWindow = null;});
+
+
+// Instanciate a new window
+function createAddWindow()
+{
+    // Starting-up notification
+    console.log('AddWindow has started');
+
+    // Create the window
+    addWindow = new BrowserWindow({ width:250, height:190, transparent: false, frame: false});
+
+    //
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    //
+    addWindow.loadURL(`file://${__dirname}/addWindow.html`);
+
+    // Destroy window
+    addWindow.on('closed', _=>{addWindow = null; console.log('AddWindow has closed');})
+}
+
+// catch item add
+ipc.on('item:add', function(e, item)
+{
+    console.log(item);
+    mainWindow.webContents.send('item:add', item)
+    addWindow.close();
+})
+
+
+const template =
+[
+    {label: electron.app.getName(), submenu: 
+        [
+            {label: 'Add Item', click: _=>{createAddWindow()}},
+            {label: 'Clear Items', click(){mainWindow.webContents.send('item:clear')}},
+            {type: 'separator'}, 
+            {label: 'Quit', click: _=>{app.quit()},accelerator: 'Ctrl+Q'}
+        ]}
+]
+
+
+// Add developer tools when developing
+if (process.env.NODE_ENV !== 'production')
+{
+    template.push({
+        label: 'Developer Tools',
+        submenu:
+        [
+            {label: 'Toggle DevTools', click(item, focusedWindow)
+            {
+                focusedWindow.toggleDevTools();
+            }},
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
+
+ipc.on('addTask', (ent, arg) =>{
+    createAddWindow();
+});
+
+
+ipc.on('item:clear', (ent, arg) =>{
+    mainWindow.webContents.send('item:clear');
+});
+
+ipc.on('app:close', (ent, arg) => {
+    app.quit();
+});
+
+ipc.on('app:account', (ent, arg) => {
+    mainWindow.webContents.send('app:account');
+});
+
+ipc.on('task:download', (ent, arg) => {
+    mainWindow.webContents.send('task:download');
+});
+
+ipc.on('task:upload', (ent, arg) => {
+    mainWindow.webContents.send('task:upload');
+});
+
+ipc.on('app:voice', (ent, arg) =>{
+    mainWindow.webContents.send('app:voice');
+});
+
+ipc.on('app:refresh', (ent, arg) =>{
+    mainWindow.webContents.send('app:refresh');
+});
